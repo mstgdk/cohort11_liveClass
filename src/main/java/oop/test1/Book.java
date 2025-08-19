aeb1663725a1daae69b8b0225768a32411e7be8b
mstgdk <mstgdk@windowslive.com>
2025-08-19 19:36:26 +0300

    19.08.2025 liveClass-1

---
+++ src/main/java/oop/test1/Book.java
@@ -0,0 +1,8 @@
+package oop.test1;
+
+public class Book {
+    private String name ="java";
+    protected String lastName="dev";
+
+
+}
---
+++ src/main/java/oop/test2/Pen.java
@@ -0,0 +1,12 @@
+package oop.test2;
+
+import oop.test1.Book;
+
+public class Pen extends Book{
+    public static void main(String[] args) {
+        Pen pen = new Pen();
+        System.out.println("pen.lastName = " + pen.lastName);
+
+
+    }
+}
